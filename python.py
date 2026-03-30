dcb5e1f5ba6e927c9b9555493da9e19a8ada3c71
mm1472 <mm1472@exeter.ac.uk>
2026-03-30 18:45:25 +0100

    Reshaping edit

--- python.py
+++ python.py
@@ -1,16 +1,20 @@
 import pandas as pd
 
-# Defining a function that reads, reshapes and renames files from wide to long formats for sheets in 5.6
+# Function that reads, reshapes and renames files from wide to long formats for sheets in 5.6
 def reshape (wide_files):
-    df = pd.read_csv(wide_files) #Load the data
+    df = pd.read_csv(wide_files) 
     
     df = df.loc[:, ~df.columns.str.contains("^Unnamed")] # drop empty, unamed columns
 
-    df_long = df.melt(
-        id_vars=["Generation type", "Fuel"],  # columns to account for 2 components in the first 2 columns
-        var_name="Year",                      # new column for years
-        value_name="Value"                    # new column for data values
-    )
+    df = df[df["Generation type"].str.strip() == "All generating companies"] # interested in total generated output
+
+    df_long = df.pivot_table(
+        # columns to account for 2 components in the first 2 columns
+        index ="Year",
+        columns = "Fuel",                      # new column for years
+        values = "Value"                    # new column for data values
+        aggfunc = "first" # WHAT DOES THIS MEAN?
+    ).reset_index()
 
     print(df_long.head())
 
