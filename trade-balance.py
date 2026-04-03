0c168e1ddccb63b3ea698c0eb5a7b85444b766cc
mm1472 <mm1472@exeter.ac.uk>
2026-04-03 21:00:05 +0100

    Retrying pivot table use once Wales and Northern Island are changed to UK

--- trade-balance.py
+++ trade-balance.py
@@ -14,6 +14,15 @@ df_long = df.melt(
     value_name="Value"
 )
 
+df_long['Type'] = (
+    df_long['Type']
+    .str.replace('Northern Ireland','UK')
+    .str.replace('Wales','UK') # Replacing NI and Wales with UK so that the formatting works with the pivot
+)
+
+df_long['Value'] = pd.to_numeric(df_long['Value'].str.replace(',', ''), errors='coerce') # Turning all characters to numeric
+#REMOVE DECIMAL PLACES FROM VALUE COLUMN/ TRUNCATE!!
+
 #Using regex to extract
 df_long["Variable"] = df_long["Type"].str.extract(r"^(Imports|Exports|Net imports)")
 df_long["Country"] = df_long["Type"].str.extract(r"\((.*?)\)") # Extracts countries trading in type column
@@ -27,24 +36,27 @@ df_long["Country"] = (
 
 #Drop rows where NaN in Variable, i.e. removing transfers within UK columns
 df_long = df_long.dropna(subset=['Variable'])
+df_final = df_long.groupby(['Year', 'Country', 'Variable'])['Value'].sum().unstack('Variable').reset_index()
 
-df_final = df_long.pivot(
+#Tried using a pivot but beause of the stacking and unstacking the import export to and from UK pivoting wasn't working, with lots of though and consulting LLMs this is what I came up with.
+df_final = df_long.pivot_table(
     index=["Year", "Country"],
     columns="Variable",
     values="Value",
+    aggfunc="sum"   # best choice for trade data acccording to AI
 ).reset_index()
 
-#Tried using a pivot but beause of the stacking and unstacking the import export to and from UK pivoting wasn't working, with lots of though and consulting LLMs this is what I came up with.
-# df_final = df_long.pivot_table(
+# df_final = df_long.pivot(
 #     index=["Year", "Country"],
 #     columns="Variable",
 #     values="Value",
-#     aggfunc="sum"   # best choice for trade data acccording to AI
 # ).reset_index()
 
 print(df_final.head())
 df_final.to_csv("net-imports-long.csv", index=False)
 
-#Data Viualsiation
-df_trade = pd.read_csv("net-imports-long.csv")
-df_trade.head()
+# #Data Viualsiation
+# df_trade = pd.read_csv("net-imports-long.csv")
+# #df_trade.head()
+
+
