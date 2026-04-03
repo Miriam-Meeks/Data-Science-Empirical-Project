64849fd80621e603fabb2279b6a0d40a3f0ad3fa
mm1472 <mm1472@exeter.ac.uk>
2026-04-03 15:02:43 +0100

    Melting dataset to have year as the id and title of the column as type, with the values in each column

--- trade-balance.py
+++ trade-balance.py
@@ -1,6 +1,14 @@
 #Trade balance
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+df = pd.read_csv("net-imports.csv")
 
 #reshape trade balance sheet to plot trade balance over time
+df_long = df.melt(
+    id_vars=["Year"],
+    var_name="Type",
+    value_name="Value"
+)
+print(df_long)
+
+
