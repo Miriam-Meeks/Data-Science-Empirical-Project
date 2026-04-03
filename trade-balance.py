330c2835d6e4c748d09ad4b8037a53b6f4ffc70b
mm1472 <mm1472@exeter.ac.uk>
2026-04-03 15:49:37 +0100

    Reshaping of net-import csv to show countries exports, imports and net for each year and saved

--- trade-balance.py
+++ trade-balance.py
@@ -1,4 +1,3 @@
-#Trade balance
 import pandas as pd
 
 df = pd.read_csv("net-imports.csv")
@@ -9,6 +8,24 @@ df_long = df.melt(
     var_name="Type",
     value_name="Value"
 )
-print(df_long)
+#Using regex to extract
+df_long["Variable"] = df_long["Type"].str.extract(r"^(Imports|Exports|Net imports)")
+df_long["Country"] = df_long["Type"].str.extract(r"\((.*?)\)") # Extracts countries trading in type column
+df_long["Country"] = (
+    df_long["Country"]
+    .str.replace(r"\s*\[.*?\]", "", regex=True) # remove notes in column
+    .str.replace(" to UK", "", regex=False) # remove "to UK"
+    .str.replace("UK to ", "", regex=False)
+    .str.strip() # useful to clean whitespace and standardise
+)
 
+# Drop rows where NaN in Variable, i.e. removing transfers within UK columns
+df_long = df_long.dropna(subset=['Variable'])
+df_final = df_long.pivot(
+    index=["Year", "Country"],
+    columns="Variable",
+    values="Value",
+).reset_index()
 
+print(df_final.head())
+df_final.to_csv("net-imports-long.csv", index=False)
