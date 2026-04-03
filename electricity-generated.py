795d377f21b6c853383e29f2d6b621556deb487d
mm1472 <mm1472@exeter.ac.uk>
2026-04-03 14:11:51 +0100

    Renaming python scripts and files for later, with new file for reshaping and visualising the trade balance

--- electricity-generated.py
+++ electricity-generated.py
@@ -1,11 +1,10 @@
 import pandas as pd
 
-
 # Function that reads, reshapes and renames files from wide to long formats for sheets in 5.6
 def reshape (wide_files):
     df = pd.read_csv(wide_files) 
     
-    df = df.loc[:, ~df.columns.str.contains("^Unnamed")] # drop empty, unamed columns
+    df = df.loc[:, ~df.columns.str.contains("^Unnamed")] # drop empty, unamed columns #EDIT THIS!!
 
     df.columns = df.columns.str.strip() #removing white space from all columns
     df["Fuel"] = df["Fuel"].astype(str).str.strip()
@@ -30,18 +29,26 @@ def reshape (wide_files):
 
     output_name = wide_files.replace(".csv", "-long.csv")
     df_final.to_csv(output_name, index=False) #saved to renamed csv
+    return df_final
 
 wide_files = ["electricity-generated.csv","share-electricity-generated.csv"]
 
-# for file in wide_files:
-#     df_[file] = reshape(file) # Working reshape :)
-    
-gen_df = reshape("electricity-generated.csv")
-share_df = reshape("share-electricity-generated.csv")
+#Altering column names for share sheet so they don't completely match electricity-generated
+df = pd.read_csv("share-electricity-generated.csv")
+df.columns = [
+    col if col in ["Year", "Fuel", "Generation type"] else col + " (%)"
+    for col in df.columns
+]
+print(df.head())
+df.to_csv("share-electricity-generated.csv", index=False)
 
-# share_df = share_df.rename(columns={
-#     col: f"{col} (%)" for col in share_df.columns if col != "Year"
-# })
+# gen_df = reshape("electricity-generated.csv")
+# share_df = reshape("share-electricity-generated.csv") # Working reshape :)
 
-# pd.merge(gen_df, share_df, on="Year", how="inner") 
+# combined_electrcity = pd.merge(gen_df, share_df, on="Year", how="inner") 
+# print(combined_electrcity.head())
+
 # # Merge can be inner as you won't have missing years not in both datasets because of the nature of the datasets.
+
+# cols = ["Year"] + sorted([col for col in df_final.columns if col != "Year"])
+# df_final = df_final[cols] # Organising alphabetically for aesthetics, will need to edit the dataset names!!
---
+++ trade-balance.py
@@ -0,0 +1,6 @@
+#Trade balance
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#reshape trade balance sheet to plot trade balance over time
